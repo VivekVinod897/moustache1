796af3af2f973d810f31b0b830ab1995a589dbb0
VivekVinod897 <76860481+VivekVinod897@users.noreply.github.com>
2021-05-13 14:46:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ poseNet.on('pose', gotPoses);
 function gotPoses(results){
 if(results.length >0){
 console.log(results);
-noseX = results[0].pose.nose.x-15;
-noseY = results[0].pose.nose.y+5;
+noseX = results[0].pose.nose.x-20;
+noseY = results[0].pose.nose.y+1;
 console.log("nose x = " + results[0].pose.nose.x);
 console.log("nose y = " + results[0].pose.nose.y);
 }
